9cf40f6d58ee0295599dd543fa538d67ff47e67b
Reiner <reiner@kyokushin.com.ph>
2012-05-25 10:25:16 +0800

    developing user right

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,8 @@
 
 class User < ActiveRecord::Base
   attr_accessible :name, :email
-
+  before_save { |user| user.email = email.downcase }
+  
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
  
