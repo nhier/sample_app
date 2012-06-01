df160465d369ef3501db85da11bf9dbcc7dfd554
Reiner <reiner@kyokushin.com.ph>
2012-06-01 15:20:49 +0800

    Finish chapter 8

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,7 +24,7 @@ describe User do
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
-  it { should respond to(:password_confirmation) }
+#  it { should respond to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) } 
   it { should be_valid }
